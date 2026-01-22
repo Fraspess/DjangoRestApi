e56b446cbde1da714e6ef085f7ac62135a6ff08f
Fraspess <pavlosamchyk@gmail.com>
2026-01-22 16:12:01 +0200

    salo

--- DjangoRestApi/atbapi/atbapi/settings.py
+++ DjangoRestApi/atbapi/atbapi/settings.py
@@ -155,10 +155,11 @@ SPECTACULAR_SETTINGS = {
     'COMPONENT_SPLIT_REQUEST': True,
 }
 
-CORS_ALLOWED_ORIGINS = os.environ.get(
-    "CORS_ALLOWED_ORIGINS",
-    "http://localhost:5173,http://127.0.0.1:5173"
-).split(",")
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = os.environ.get(
+#     "CORS_ALLOWED_ORIGINS",
+#     "http://localhost:5173,http://127.0.0.1:5173"
+# ).split(",")
 
 DJOSER = {
     'LOGIN_FIELD': 'email',
